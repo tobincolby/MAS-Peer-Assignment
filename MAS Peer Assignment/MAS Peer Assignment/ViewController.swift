b8b6f91f6d2a6e007d8b3b703ed94b22ab2d7348
Kar Shin Lin <lin.karshin@gmail.com>
2019-01-23 22:26:36 -0500

    Adding hardcoded master username and pass (to be removed later). Added error messaging for wrong login information.

--- MAS Peer Assignment/MAS Peer Assignment/ViewController.swift
+++ MAS Peer Assignment/MAS Peer Assignment/ViewController.swift
@@ -12,12 +12,17 @@ import FirebaseDatabase
 class ViewController: UIViewController {
 
     var ref: DatabaseReference!
+    let masterUsername: String = "master"
+    let masterPass: String = "pass1234"
     
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
+        errorLabel.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,11 +30,12 @@ class ViewController: UIViewController {
         
         let username: String = usernameText.text as String!
         let password: String = passwordText.text as String!
-        
-        print(username)
-        print(password)
-        
-        
+        if masterUsername == username && masterPass == password {
+            self.performSegue(withIdentifier: "loginUser", sender: nil)
+        } else {
+            errorLabel.text = "Wrong username or password. Please try again."
+            print("Wrong username or password. Please try again")
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
