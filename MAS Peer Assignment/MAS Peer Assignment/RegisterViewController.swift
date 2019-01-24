ee9f5b12167ae27bf268028847e845a7d906f662
Colby Tobin <>
2019-01-23 22:38:48 -0500

    merge in location requests

--- MAS Peer Assignment/MAS Peer Assignment/RegisterViewController.swift
+++ MAS Peer Assignment/MAS Peer Assignment/RegisterViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 import FirebaseDatabase
+import CoreLocation
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, CLLocationManagerDelegate {
     
+    let locationMgr = CLLocationManager()
     var ref: DatabaseReference!
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var confirmPassText: UITextField!
+    var location: CLLocation?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +29,37 @@ class RegisterViewController: UIViewController {
 
     @IBAction func registerUser(_ sender: Any) {
     
+        let status  = CLLocationManager.authorizationStatus()
+        
+        // 2
+        if status == .notDetermined {
+            locationMgr.requestWhenInUseAuthorization()
+            return
+        }
+        
+        // 3
+        if status == .denied || status == .restricted {
+            let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable Location Services in Settings", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // 4
+        locationMgr.delegate = self
+        locationMgr.startUpdatingLocation()
         let username: String = usernameText.text as! String
         let password: String = passwordText.text as! String
         let confirmPass: String = confirmPassText.text as! String
+        
+        let latitude = location!.coordinate.latitude
+        let longitude = location!.coordinate.longitude
+        
+        
+        
         
         print(username)
         print(password)
@@ -37,6 +69,17 @@ class RegisterViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations.last!
+        location = currentLocation
+        print("Current location: \(currentLocation)")
+    }
+    
+    // 2
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
     }
     
 
